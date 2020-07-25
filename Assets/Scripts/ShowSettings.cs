87563ad797cd2b3b9428f3f437b753acb1478c5d
mixxor-project <mixxorproject@gmail.com>
2020-07-25 20:58:55 +0900

    게임시작 시 챕터1로 연결과 환경설정 panel 추가

---
+++ Assets/Scripts/ShowSettings.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowSettings : MonoBehaviour
+{
+    public void ShowSettingMenu(GameObject settings)
+    {
+        settings.SetActive(true);
+    }
+    public void HideSettingMenu(GameObject settings)
+    {
+        settings.SetActive(false);
+    }
+}
