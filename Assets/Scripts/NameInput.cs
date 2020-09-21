6c13d5fcd3a2f8508d83c6464584e3e6ff64b341
mixxor-project <mixxorproject@gmail.com>
2020-09-21 22:26:11 +0900

    이름 입력 씬 추가

---
+++ Assets/Scripts/NameInput.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NameInput : MonoBehaviour
+{
+    public InputField name;
+
+    public void PrintName()
+    {
+        var _name = name.text;
+        Debug.Log(_name);
+    }
+}
