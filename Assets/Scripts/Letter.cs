9501dca86949caae3c476d558f2d09d199aba182
mixxor-project <mixxorproject@gmail.com>
2020-10-17 20:32:40 +0900

    편지 일러스트 추가

---
+++ Assets/Scripts/Letter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Letter : MonoBehaviour
+{
+    private Text myText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myText = GameObject.FindGameObjectWithTag("PeopleText").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(myText.text == "(황실에서 온 급한 편지다. 얼른 읽어보자)")
+        {
+            GameObject.Find("Image").transform.FindChild("Letter").gameObject.SetActive(true);
+        }    
+
+        if(myText.text == "~튜토리얼~")
+        {
+            GameObject.Find("Image").transform.FindChild("Letter").gameObject.SetActive(false);
+        }
+    }
+}
