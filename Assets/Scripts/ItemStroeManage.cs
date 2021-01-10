5c85af7c496fd01b66bfd1319625a97b714748d4
mixxor-project <mixxorproject@gmail.com>
2021-01-10 19:25:40 +0900

    Feat : 수정

--- Assets/Scripts/ItemStroeManage.cs
+++ Assets/Scripts/ItemStroeManage.cs
@@ -1,24 +1,20 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
-
-[System.Serializable]
-public class BackgroundImgs
-{
-    public string name;
-    public Sprite img;
-}
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ItemStroeManage : Singleton<ItemStroeManage>
 {
+    List<string> itemList = new List<string>();
     public static ItemStroeManage instance;
-
-    public List<BackgroundImgs> bgImgList;
 
     private void Awake()
     {
         instance = this;
+        PlayerPrefs.DeleteAll();
     }
 
     public bool ReadTextData(int _chapterNum, int _sceneNum)
@@ -54,5 +50,68 @@ public class ItemStroeManage : Singleton<ItemStroeManage>
     void SelectItem() // 아이템 구입 버튼 클릭 이벤트
     {
 
+    }
+
+    public void ExampleFun()
+    {
+        // Button 눌렸을 때 button > itemName > text 가져오는 ...
+        var itemName = EventSystem.current.currentSelectedGameObject.GetComponent<Button>().gameObject.transform.Find("ItemName").GetComponent<Text>().text;
+
+        if (overlapChk(itemName)) // 이미 저장되었을 때
+        {
+            Debug.Log("이미 저장되어있는 아이템입니다.");
+        }
+        else // 저장되어있지 않을 때 
+        {
+            string strArr = ""; // 문자열 생성
+
+            foreach(var item in itemList)// 배열과 ','를 번갈아가며 tempStr에 저장
+            {
+                if (item.Length > 0)
+                {
+                    strArr = strArr + item;
+                    strArr = strArr + ",";
+                }
+            }
+
+            strArr = strArr + itemName;
+
+            print("아이템이 성공적으로 저장되었습니다.\n저장된 아이템들 : " + strArr); // 저장된 items 보기
+
+            PlayerPrefs.SetString("Data", strArr); // PlyerPrefs에 문자열 형태로 저장
+        }
+    }
+
+    public bool overlapChk(string itemName)
+    {
+        if (getItems()) // 값이 있으면
+        {
+            return itemList.Contains(itemName);
+        }
+        return false;
+    }
+
+    public Boolean getItems()
+    {
+        itemList.Clear();
+        var itemListTmp = PlayerPrefs.GetString("Data").Split(',');
+
+        if (itemListTmp.Length > -1)
+        {
+            int[] number2 = new int[itemListTmp.Length]; // 문자열 배열의 크기만큼 정수형 배열 생성
+
+            for (int i = 0; i < itemListTmp.Length; i++)
+            {
+                itemList.Add(itemListTmp[i]);
+            }
+
+            return true;
+        }
+        return false;
+    }
+
+    public void saveItem() // 아이템 저장하는 함수
+    {
+        
     }
 }
